5caa70d71ad971524c9e8c65fd6eaf5abe77f5e9
Sunny Pukkalli <pukkallisunny@gmail.com>
2018-07-18 23:13:08 +0530

    First Commit

---
+++ src/main/java/com/app/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	
+	@RequestMapping("/main")
+	public ModelAndView hello(){
+		
+		ModelAndView map = new ModelAndView("Main");
+		map.addObject("message", "Hello World");
+		return map;
+	}
+	
+	@RequestMapping("/")
+	public String main(){
+		
+		return "index";
+	}
+}
